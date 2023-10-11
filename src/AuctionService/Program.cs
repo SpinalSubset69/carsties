3e97ff97dd4fe1b6cc35ce3afc9b21147ae8ff5e
spinalsubet69 <alastorlml@gmail.com>
2023-10-11 09:47:39 -0600

    End of section 2

---
+++ src/AuctionService/Program.cs
@@ -0,0 +1,30 @@
+using AuctionService.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+builder.Services.AddDbContext<AuctionDbContext>(options => 
+{
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+// Look for any class that inherits from Profile
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+// Seed Database
+try{
+    DbInitializer.InitDb(app);
+}catch(Exception e){
+    Console.WriteLine(e);
+}
+
+app.Run();
